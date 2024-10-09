d0b49012cbcd75cd9d9cea21ab7944f3d087d6aa
Iman Syaefulloh <imansyaefulloh@gmail.com>
2024-10-10 00:07:29 +0700

    http response with html and json

---
+++ src/main/java/com/imansyaefulloh/servlet/BodyServlet.java
@@ -0,0 +1,25 @@
+package com.imansyaefulloh.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/body")
+public class BodyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String html = """
+            <html>
+            <body>
+            <h1>Hello Iman Syaefulloh </h1>
+            </body>
+            </html>
+            """;
+
+        resp.getWriter().println(html);
+    }
+}
---
+++ src/main/java/com/imansyaefulloh/servlet/JsonServlet.java
@@ -0,0 +1,24 @@
+package com.imansyaefulloh.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/json")
+public class JsonServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String json = """
+            {
+                "name": "Iman",
+                "value": 100
+            }
+            """;
+        resp.setHeader("Content-Type", "application/json");
+        resp.getWriter().println(json);
+    }
+}
