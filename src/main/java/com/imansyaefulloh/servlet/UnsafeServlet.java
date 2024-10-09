6ce36ab628f48930257b8231bb8485cb2442e3e8
Iman Syaefulloh <imansyaefulloh@gmail.com>
2024-10-09 23:30:57 +0700

    fix race condition issues

--- src/main/java/com/imansyaefulloh/servlet/UnsafeServlet.java
+++ src/main/java/com/imansyaefulloh/servlet/UnsafeServlet.java
@@ -11,14 +11,16 @@ import java.io.IOException;
 @WebServlet(urlPatterns = "/unsafe")
 public class UnsafeServlet extends HttpServlet {
 
-    private String response = "";
+    // this will trigger race condition issue
+    // NEVER declare sharing variable in here
+//    private String response = "";
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
         Long sleep = Long.parseLong(req.getParameter("sleep"));
 
-        response = "Hello " + name + "!";
+        String response = "Hello " + name + "!";
 
         try {
             Thread.sleep(sleep);
